1c40a9985490255573847f198b2a433629bc974b
Rayan Aouf <rayanaouf1512@gmail.com>
2024-07-18 23:27:14 +0100

    feat: chifrage doctype

---
+++ halfware/halfware/doctype/chiffragehardware/test_chiffragehardware.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, rayan aouf  and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestChiffrageHardware(FrappeTestCase):
+	pass
---
+++ halfware/halfware/doctype/chiffrageservice/test_chiffrageservice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, rayan aouf  and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestChiffrageService(FrappeTestCase):
+	pass
