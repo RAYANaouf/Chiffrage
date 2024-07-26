d731c40d3ecaa849aa485f69e06c347687733ee5
Rayan Aouf <rayanaouf1512@gmail.com>
2024-07-26 01:53:59 +0100

    chifrage V0.03

--- halfware/halfware/doctype/chiffragehardware/chiffragehardware.js
+++ halfware/halfware/doctype/chiffragehardware/chiffragehardware.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, rayan aouf  and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("ChiffrageHardware", {
-// 	refresh(frm) {
+ frappe.ui.form.on("ChiffrageHardware", {
+ 	refresh(frm) {
 
-// 	},
-// });
+ 	},
+
+       { unit_price} : function(frm , cdt , cdn){
+
+           frappe.msgprint("hello rayan")
+
+        }
+ });
